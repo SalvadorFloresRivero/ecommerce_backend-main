21e18e792fc65cf0e4b701563392224f45b3ec79
SalvadorFloresRivero <salvador.flores@insabi.gob.mx>
2022-11-22 09:56:27 -0600

    Primer commit

---
+++ controllers/product.controllers.js
@@ -0,0 +1,77 @@
+
+const { Product } = require('../models');
+
+const productsGet = async(req, res) =>{
+
+    const { desde = 0, limite = 8 } = req.query;
+
+    const query = { status: true};
+
+    const [ products, total ] = await Promise.all([
+        Product.find(query)
+        .populate('category', 'name')
+        .skip(Number(desde)).limit(Number(limite)),
+        Product.countDocuments(query)
+    ])
+
+    res.json({ products, total })
+}
+
+
+const productsGetById = async(req, res) =>{
+    const { id } = req.params;
+    const product = await Product.findById(id).populate('category','name');
+
+    res.json(product);
+}
+
+
+const productsPost = async (req, res) =>{
+
+    const {status,  name, ...resto} = req.body;
+
+    const productDB = await Product.findOne({ name });
+
+    if(productDB){
+        return res.status(400).json({msg: `product ${name} exist`});
+    }
+
+    const data = {
+        ...resto,
+        name
+    }
+
+    const product = new Product(data);
+
+    await product.save();
+
+    res.status(201).json(product);
+
+}
+
+
+const productsPut = async(req, res) =>{
+    const { id } = req.params;
+    const {status,  ...data} = req.body;
+
+    const product = await Product.findByIdAndUpdate(id, data);
+
+
+    res.json({ 'msg': 'put', product })
+}
+
+
+const productsDelete = async (req, res) =>{
+    const { id } = req.params;
+
+    const  product = await Product.findByIdAndUpdate(id, { status :false, available: false });
+    res.json({ 'msg': 'delete', product })
+}
+
+module.exports = {
+    productsGet,
+    productsGetById,
+    productsPost,
+    productsPut,
+    productsDelete
+}
---
+++ models/product.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+
+const ProductSchema = Schema({
+    name: {
+        type: String,
+        required: ['name required']
+    },
+    description: {
+        type: String
+    },
+    status: {
+        type: Boolean,
+        default: true
+    },
+    imgsUrl: {
+        type: Array,
+        default: ''
+    },
+    price: {
+        type: Number,
+        default: 0
+    },
+    discount:{
+        type: Boolean,
+        default: false
+    },
+    discount_percentage:{
+        type: Number,
+        default: 0
+    },
+    stock:{
+        type: Number,
+        default: 0
+    }
+
+})
+
+ProductSchema.methods.toJSON = function () {
+    const { __v, status, ...data } = this.toObject();
+
+    return data;
+}
+
+
+module.exports = model('Product', ProductSchema);
